b47f0938ac27285ef21bb50f968c391ddea0d267
corranmac <corranmac@hotmail.co.uk>
2022-12-05 20:06:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,39 @@
 from transformers import pipeline
 import torch
 
+
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
+
+
 def init():
-    global model
-    
+    import pandas as pd 
+    import requests, io
     device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
-def inference(model_inputs:dict) -> dict:
-    global model
+def inference(inputs:dict) -> dict:
+    path= inputs.get("path",None)
+    pdf= inputs.get("pdf",None)
+    if path == None:
+        return {'message': "No path provided"}
 
-    # Parse out your arguments
-    prompt = model_inputs.get('prompt', None)
-    if prompt == None:
-        return {'message': "No prompt provided"}
-    
-    # Run the model
-    result = model(prompt)
-
+    if pdf == "bytes":
+        try: 
+            files = {"pdf_file": ("pdf.pdf", pdf, "multipart/form-data")}
+            r = requests.post('http://localhost:8080/parse', files=files)
+            parsed = pd.read_csv(io.StringIO(r.content.decode('utf-8')))
+        except:
+            return {'message': "Invalid pdf bytes"}
+    elif pdf =="url":
+        try:
+            relative_coordinates = True # whether returning relative coordinates or not 
+            parsed = pd.read_csv(f"http://127.0.0.1:8080/parse/?pdf_url={path}&relative_coordinates={relative_coordinates}")
+        except:
+            return {'message': "Invalid url or pdf at url"}
+    else:
+      return {'message': "Invalid pdf"}
+    result= {'pd_df":parsed}
     # Return the results as a dictionary
     return result
